14b00aa2d88061bebfbc194142b6d0f2b6d7badd
Shereen Mohamed <shereen.khalafallah@gmail.com>
2024-12-05 16:10:34 +0200

    Adding flex wrap

--- public/js/components.js
+++ public/js/components.js
@@ -1,6 +1,6 @@
 export function buildHeader(navItems) {
   return `<div class="container">
-        <nav class="flex">
+        <nav class="flex row wrap">
           <h1 id="logo">Codebyte</h1>
           <ul class="flex">
             <li><a   href="#">Login</a></li>
