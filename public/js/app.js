82cd45f395381f63dda88c3981326bfe1adfc2c4
Shereen Mohamed <shereen.khalafallah@gmail.com>
2024-12-10 22:18:00 +0200

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,30 @@
-import { buildHeader } from "./components.js";
+// import { buildHeader } from "./components.js";
 
-(function init() {
-  const header = document.querySelector("header");
-  header.innerHTML = buildHeader();
-})();
-// IIFE -- Immediately Invoked Function Expression
+// (function init() {
+//   const header = document.querySelector("header");
+//   header.innerHTML = buildHeader();
+// })();
+// // IIFE -- Immediately Invoked Function Expression
+const isAuthenticate = true;
+const Menu = document.querySelector(".menu");
+document.addEventListener("DOMContentLoaded", () => {
+  if (isAuthenticate) {
+    Menu.innerHTML = ` <li>
+              <a><i class="fa-solid fa-right-from-bracket"></i></a>
+            </li>
+            <li>
+              <a><i class="fa-solid fa-bell"></i></a>
+            </li>
+            <li>0 xp</li>
+            <li>
+              <a><img src="https://placehold.co/400" alt="" /></a>
+            </li>`;
+  } else {
+    Menu.innerHTML = `<li>
+              <a>Login</a>
+            </li>
+            <li>
+              <a>Signup</a>
+            </li>`;
+  }
+});
--- public/js/components.js
+++ public/js/components.js
@@ -1,27 +1,27 @@
-export function buildHeader(navItems) {
-  return `<div class="container">
-        <nav class="flex row wrap">
-          <h1 id="logo">Codebyte</h1>
-
-          <ul class="toggle-menu ">
-            <li><input type="text" placeholder="search" /></li>
-            <li><i class="fas fa-bars toggle-menu"></i></li>
-          </ul>
-
-          
-          <ul class=" menu flex">
-            <li><input type="text" placeholder="search" /></li>
-            <li>
-              <a><i class="fa-solid fa-bell"></i></a>
-            </li>
-            <li><a href="#">Profile</a></li>
-            <li><a href="#">Challenges</a></li>
-            <li>
-              <a href="#"
-                >Logout <i class="fa-solid fa-right-from-bracket"></i
-              ></a>
-            </li>
-          </ul>
-        </nav>
-      </div>`;
-}
+// export function buildHeader(navItems) {
+//   return `<div class="container">
+//         <nav class="flex row wrap">
+//           <h1 id="logo">Codebyte</h1>
+//           <ul class="menu flex">
+//             <li>
+//               <a><i class="fa-solid fa-right-from-bracket"></i></a>
+//             </li>
+//             <li>
+//               <a><i class="fa-solid fa-bell"></i></a>
+//             </li>
+//             <li>0 xp</li>
+//             <li>
+//               <a><img src="https://placehold.co/400" alt="" /></a>
+//             </li>
+//           </ul>
+//           <ul class="menu flex">
+//             <li>
+//               <a>Login</a>
+//             </li>
+//             <li>
+//               <a>Signup</a>
+//             </li>
+//           </ul>
+//         </nav>
+//       </div>`;
+// }
