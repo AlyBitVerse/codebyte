72a3b07a0209c371235c29acc10c6952551bb449
Mohamed Aly <troy100100@gmail.com>
2024-12-12 18:08:19 +0200

    Refactor login/register form structure

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,43 +1,27 @@
-const auth = document.getElementById("auth");
-const logIn = () => {
-  auth.innerHTML = `<section id="log-in" class="log-in">
-    <h1>Log <span>in</span></h1>
-      <form action="" class="flex col">
-        <input type="text" placeholder="Email" />
-        <input type="text" placeholder="Password" />
-        <p>
-          Don't have an account ? <a id="signup-btn" >Sign up</a> <br />
-          <span>Forget your password ?</span>
-        </p>
-        <a class="btn">Continue</a>
-      </form>
-      </section>`;
-  const signupBtn = document.getElementById("signup-btn");
-  signupBtn.addEventListener("click", signUp);
-};
-const signUp = () => {
-  auth.innerHTML = `<section id="sign-up" class="sign-up"><h1>Sign <span>up</span></h1>
-    <form action="" class="flex col">
-      <input type="text" placeholder="Name" />
-      <input type="text" placeholder="User name" />
-      <input type="text" placeholder="Email" />
-      <input type="text" placeholder="Password" />
-      <input type="text" placeholder="Repeat the password" />
-      <div class="check">
-        <input type="checkbox" name="" id="" />
-        <label for="">
-          I agree to the <span>Terms&conditions</span> and
-          <span>Privacy Policy</span>
-        </label>
-      </div>
+const authContainer = document.getElementById("authContainer");
 
-      <a class="btn">Create account</a>
-      <a class="btn" id="login-btn"> Log In</a>
-    </form>
-</section>`;
-  const loginBtn = document.getElementById("login-btn");
+const registerForm = document.getElementById("register-form");
+const loginForm = document.getElementById("login-form");
 
-  loginBtn.addEventListener("click", logIn);
-};
+// Switch between login and register forms
+document.getElementById("show-register").addEventListener('click', () => {
+  registerForm.classList.remove('hidden');
+  loginForm.classList.add('hidden');
 
-signUp();
+});
+
+document.getElementById("show-login").addEventListener('click', () => {
+  loginForm.classList.remove('hidden');
+  registerForm.classList.add('hidden');
+});
+
+function login() {
+  // HTTP Request
+}
+
+function register() {
+  // HTTP Request
+}
+
+document.getElementById('login-form').addEventListener('submit', login);
+document.getElementById('register-form').addEventListener('submit', register);
