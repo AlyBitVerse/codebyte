9ab4154bc23fd983871cfb939395cf2551be0a48
Shereen Mohamed <shereen.khalafallah@gmail.com>
2024-12-15 01:11:42 +0200

    Update challenges styling

--- public/js/challenges.js
+++ public/js/challenges.js
@@ -1,6 +1,39 @@
 const BASE_URL = "http://localhost:3000";
 const API_BASE_URL = "http://localhost:3000/api";
+const selections = document.querySelector(".selections-menu");
+function renderMenu() {
+  selections.innerHTML = `
+  <form class="flex col">
+        <select name="lang" id="">
+          <option value="">JavaScript</option>
+          <option value="">Python</option>
+          <option value="">PHP</option>
+          <option value="">C#</option>
+          <option value="">C++</option>
+          <option value="">Java</option>
+        </select>
+        <select name="level" id="">
+          <option value="">Very Easy</option>
+          <option value="">Easy</option>
+          <option value="">Meduim</option>
+          <option value="">Hard</option>
+          <option value="">Very Hard</option>
+          <option value="">Expert</option>
+        </select>
 
+        <input list="Tags" name="tags" id="tags" placeholder="Tags (Optional)" />
+        <datalist id="Tags">
+          <option value="Loops"></option>
+          <option value="Conditions"></option>
+          <option value="Functions"></option>
+          <option value="Strings"></option>
+          <option value="Arrays"></option>
+          <option value="Numbers"></option>
+        </datalist>
+        <img src="https://placehold.co/400" alt="">
+      </form>`;
+}
+renderMenu();
 const challengesContainer = document.querySelector(".challenges");
 
 function renderChallenges() {
