605b60a1cdae275fa1908d4cfe3efa0b38b2106a
Shereen Mohamed <shereen.khalafallah@gmail.com>
2024-12-18 21:35:37 +0200

    Improve navbar stylting and add cookies for authentication

--- public/js/app.js
+++ public/js/app.js
@@ -1,31 +1,39 @@
-// import { buildHeader } from "./components.js";
-
-// (function init() {
-//   const header = document.querySelector("header");
-//   header.innerHTML = buildHeader();
-// })();
-// // IIFE -- Immediately Invoked Function Expression
-const isAuthenticate = false;
+const BASE_URL = "http://localhost:3000";
+const API_BASE_URL = "http://localhost:3000/api";
 const Menu = document.querySelector(".menu");
-function renderNavbar ()  {
-  if (isAuthenticate) {
-    Menu.innerHTML = ` <li>
-              <a><i class="fa-solid fa-right-from-bracket"></i></a>
+
+function renderNavbar() {
+  if (Cookies.get("token")) {
+    console.log("Token is present:", Cookies.get("token"));
+    Menu.innerHTML = ` 
+            <li>
+              <a ><i id="logoutIcon" class="fa-solid fa-right-from-bracket" ></i></a>
             </li>
             <li>
               <a><i class="fa-solid fa-bell"></i></a>
             </li>
-            <li>0 xp</li>
+            <li class='xp'>0 xp</li>
             <li>
-              <a href="../pages/profile.html"><img src="https://placehold.co/400" alt="" /></a>
+              <a href="../pages/profile.html" class="userImage"><img src="https://placehold.co/400" alt="" /></a>
             </li>`;
   } else {
-    Menu.innerHTML = `<li>
+    console.log("Token is not present.");
+    Menu.innerHTML = `
+            <li>
               <a href="../pages/auth.html">Login</a>
             </li>
-            <li>
-              <a href="../pages/auth.html">Signup</a>
-            </li>`;
+           `;
   }
-};
-renderNavbar()
+}
+renderNavbar();
+
+function handelLogout(event) {
+  event.preventDefault();
+  document.cookie =
+    "token=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT; secure; HttpOnly;";
+  // Redirect to homepage
+  window.location.href = "/";
+  // Re-render the navbar to reflect the updated state
+  renderNavbar();
+}
+document.getElementById("logoutIcon").addEventListener("click", handelLogout);
